4af15ab7edaa2494b96143f484fc4b76bdccd66d
Neal Benner <kyrandita@gmail.com>
2017-09-23 20:03:20 -0600

    still setting stuff up, trying to figure out karma and jasmine...

---
+++ index.mjs
@@ -0,0 +1,3 @@
+export function expandPattern(pattern) {
+    return pattern;
+}
---
+++ test/number-range.spec.js
@@ -0,0 +1,5 @@
+describe('expandPattern', function() {
+	it('should expand a numeric range in increasing and decreasing order', function() {
+		expect(expandPattern('pattern')).toEqual('pattern');
+	});
+});
