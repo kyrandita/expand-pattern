a07656534728213219461a98b32672005237a012
Neal Benner <kyrandita@laurence.bennerink>
2017-10-09 23:04:39 -0600

    got a better version of parse theoretically in place, calc is next

--- index.js
+++ index.js
@@ -1,56 +1,143 @@
 
-let ExpPatRange = (function(){
-  let obj = {};
-  obj.prototype = Array;
-  return obj;
-})();
+function StringToken(val){
+  this.value = val;
+}
+function StartToken(){}
+function EndToken(){}
+function ColonToken(){}
+function CommaToken(){}
 
-function tokenize(pattern) {
-  //TODO preferrably the array should contain correctly typed tokens, meaning we'll likely have to make all the relevant types
-  return pattern;
+function SetObject(...args){
+  this.setThings = args;
+}
+function RangeObject(startString, endString, step = 1, characterSet){
+  this.start = startString;
+  this.end = endString;
+  this.step = step;
+  this.set = characterSet;
+}
+function NumericRangeObject(startString, endString, step = 1, base = 10){
+  this.start = startString;
+  this.end = endString;
+  this.step = step;
+  this.base = base;
+}
+function Context(tokens){
+  this[Symbol.iterator] = function(){return this;};
+  Object.defineProperty(this, 'tokens', {get: function(){return tokens;}});
+  this.next = function(){
+    return {
+      value:tokens[this.curIndex++],
+      done: tokens.length < this.curIndex
+    };
+  };
+  this.peek = function(){return tokens[this.curIndex];};
+  this.curIndex = 0;
 }
 
-function parse(tokens){
+function tokenize(pattern) {
+  let runningString = '';
+  let tokenList = [];
+  for(let char of pattern) {
+    // console.log(char);
+  }
+  //return tokenList;
+  //TODO preferrably the array should contain correctly typed tokens, meaning we'll likely have to make all the relevant types
+  return [new StringToken('str'), new StartToken(), new StringToken('1'), new ColonToken(':'), new StringToken('3'), new CommaToken(','), new StringToken('4'), new EndToken('}'), new StringToken('ing')];
+}
+
+function parse(context){
   //This code was done before tokenize was split into it's own function and will be rewritten to work with the output of tokenize
   let processArray = [];
-  let tokenList = tokens;
-  let currentToken = null;
-  while(tokenList.length > 0) {
-    let lIndex = tokenList.indexOf('{');
-    let rIndex = tokenList.indexOf('}');
-    let cIndex = tokenList.indexOf(':');
-    if(cIndex < lIndex && lIndex < rIndex) {
-      throw 'error';
-    }
-    if (cIndex > -1 && (rIndex < 0 || cIndex < rIndex) && (lIndex < 0 || cIndex < lIndex)) {
-      processArray.push(tokenList.slice(0,cIndex));
-      tokenList = tokenList.slice(cIndex+1);
-    } else if (lIndex > -1 && (rIndex < 0 || lIndex < rIndex)) {
-      processArray.push(tokenList.slice(0,lIndex));
-      let subArray = parse(tokenList.slice(lIndex+1));
-      tokenList = subArray.pop();
-      processArray.push(subArray);
-    } else if (rIndex > -1 && (lIndex < 0 || lIndex < rIndex)) {
-      processArray.push(tokenList.slice(0,rIndex));
-      processArray.push(tokenList.slice(rIndex+1));
-      tokenList = '';
-    } else {
-      processArray.push(tokenList);
-      tokenList = '';
+  let state = Symbol.for('string');
+  for(let token of context) {
+    if(token instanceof StringToken) {
+      processArray.push(token.value);
+    } else if(token instanceof StartToken) {
+      processArray.push(parseRange(context));
     }
   }
-  return processArray;
+  return new SetObject(...processArray);
 };
 
+function parseRange(context) {
+  let firstToken = context.next().value;
+  if (firstToken instanceof StartToken){
+    firstToken = parseRange(context);
+  }
+  let secondToken = context.next().value;
+  if (secondToken instanceof CommaToken) {
+    let setPieces = [firstToken];
+    for(let token of context) {
+      if(token instanceof StartToken) {
+        setPieces.push(parseRange(context));
+      } else if (token instanceof StringToken) {
+        setPieces.push(token.value);
+      } else if (token instanceof EndToken) {
+        return SetObject(...setPieces);
+      }
+    }
+  } else if (secondToken instanceof ColonToken) {
+    let rangeStart = firstToken.value, rangeEnd = null, stepChar = null, numericBase = 10;
+    let tempToken = context.next();
+    let numeric = false;
+    if(tempToken.done || !tempToken.value instanceof StringToken) {
+      throw 'ERROR: range must have at least beginning and end of range';
+    }
+    rangeEnd = tempToken.value.value;
+    tempToken = context.next();
+    if(tempToken.done) {
+      throw 'error';
+    }
+    if(tempToken.value instanceof ColonToken) {
+      tempToken = context.next();
+      if(tempToken.done || !tempToken.value instanceof StringToken) {
+        throw 'error';
+      }
+      stepChar = rangeEnd;
+      rangeEnd = tempToken.value.value;
+      tempToken = context.next();
+    }
+    if(tempToken.done) {
+      throw 'error';
+    }
+    if(tempToken.value instanceof CommaToken) {
+      numeric = true;
+      tempToken = context.next();
+      if(tempToken.done || !tempToken.value instanceof StringToken) {
+        throw 'error';
+      }
+      numericBase = Number.parseInt(tempToken.value.value);
+      tempToken = context.next();
+    }
+    if (tempToken.done || !tempToken.value instanceof EndToken) {
+      throw 'error';
+    }
+    if(numeric || Number.parseFloat(rangeStart).toString(numericBase) == rangeStart && Number.parseFloat(rangeEnd).toString(numericBase) == rangeEnd) {
+      let step = stepChar? Math.abs(Number.parseFloat(rangeStart) - Number.parseFloat(stepChar)) : 1;
+      return new NumericRangeObject(Number.parseFloat(rangeStart), Number.parseFloat(rangeEnd), step, numericBase);
+    }
+    //TODO find set start and end are from, if step check that also
+    return new RangeObject();
+  } else {
+    throw 'bad crap happened';
+  }
+}
+
 function calc(parseTree) {
+  let totalArray = [];
+  console.log(parseTree);
+  if(!parseTree instanceof SetObject && !parseTree instanceof RangeObject && !parseTree instanceof NumericRangeObject) {
+    throw 'error';
+  }
   return parseTree;
 }
 
 export function expandPattern(pattern) {
     if(pattern.length > 0 && pattern.indexOf('{') > -1) {
-      let tokens = calc(parse(tokenize(pattern)));
+      let tokens = calc(parse(new Context(tokenize(pattern))));
       return tokens;
     } else {
-      return pattern;
+      return [pattern];
     }
 }
--- test/number-range.spec.js
+++ test/number-range.spec.js
@@ -1,6 +1,7 @@
 import {expandPattern} from '../index';
 describe('expandPattern', function() {
 	it('should expand a numeric range in increasing and decreasing order', function() {
+		expect(expandPattern('string')).toEqual(['string']);
 		expect(expandPattern('str{1:3,4}ing')).toEqual(['str1ing', 'str2ing', 'str3ing']);
 		expect(expandPattern('str{1,{a:b},3}ing')).toEqual(['str1ing', 'str2ing', 'str3ing']);
 	});
